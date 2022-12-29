ea910c0737096187c1c0f20f793c79331e93d2f7
devmoi <moishadrack.com>
2022-12-29 21:46:22 +0300

    add

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+export function About() {
+  return <>
+  <Container>
+  <h1 className="text-center">About</h1>
+  <p>This is a Cart web App</p>
+  </Container>
+  </>
+  
+}
