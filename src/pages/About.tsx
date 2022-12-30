ea259704d4ebccfb66ae8335e21f8e5413bf8c49
devmoi <moishadrack.com>
2022-12-30 13:44:21 +0300

    add images

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,9 +2,15 @@ import { Container } from "react-bootstrap";
 
 export function About() {
   return <>
-  <Container>
   <h1 className="text-center">About</h1>
+  <Container className="d-flex justify-content-between align-items-baseline mb-4">
+  
+<div className="p" style={{margin:0 }}>
   <p>This is a Cart web App</p>
+  </div>
+  <div className="image">
+  <p>This is a Cart web App</p>
+  </div>
   </Container>
   </>
   
