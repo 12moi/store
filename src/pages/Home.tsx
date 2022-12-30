3921b6caed85c9dd65c5cf94959667cda03a04ea
devmoi <moishadrack.com>
2022-12-30 14:53:35 +0300

    add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import { Button, Container } from "react-bootstrap";
+
 export function Home() {
-  return <h1>Home</h1>
+  return<>
+  <h1 className="text-center">Welcome to Store</h1>
+  <Container className="d-flex justify-content-between align-items-center ">
+  
+<div className="p" >
+  <p>This is your shopping cart to add the list of your product to be bought.</p>
+  <h6>You need a Cart for your shopping ,</h6>
+  <h5>We got you covered</h5>
+  <a href="/store"><Button>Store</Button></a>
+  </div>
+  <div className="image">
+  <img src="/public/imgs/4.jpg" alt="#" style={{width: "500px", height:"400px"}}/>
+  </div>
+  </Container>
+  </>
 }
