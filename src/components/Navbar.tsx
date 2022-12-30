962b2005c8e61204d19134b70e4531463213f251
devmoi <moishadrack.com>
2022-12-30 18:20:34 +0300

    add updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,15 @@ export function Navbar() {
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>
+          {/* <Nav.Link to="/" as={NavLink}>
             Home
-          </Nav.Link>
+          </Nav.Link> */}
           <Nav.Link to="/store" as={NavLink}>
             Store
           </Nav.Link>
-          <Nav.Link to="/about" as={NavLink}>
+          {/* <Nav.Link to="/about" as={NavLink}>
             About
-          </Nav.Link>
+          </Nav.Link> */}
         </Nav>
         {cartQuantity > 0 && (
           <Button
