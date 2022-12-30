97362778947f8f025854bae3caa9a64da241743c
devmoi <moishadrack.com>
2022-12-30 14:07:05 +0300

    add changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,10 +3,12 @@ import { Button, Container } from "react-bootstrap";
 export function About() {
   return <>
   <h1 className="text-center">About</h1>
-  <Container className="d-flex justify-content-between align-items-baseline mb-4">
+  <Container className="d-flex justify-content-between align-items-center ">
   
-<div className="p" style={{margin:0 }}>
-  <p>This is a Cart web App</p>
+<div className="p" >
+  <p>This is your shopping cart to add the list of your product to be bought.</p>
+  <h6>You need a Cart for your shopping ,</h6>
+  <h5>We got you covered</h5>
   <a href="/store"><Button>Store</Button></a>
   </div>
   <div className="image">
