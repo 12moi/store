91a8f4cdb0432f0a4d8c4b9c91280d38b2f45d71
devmoi <moishadrack.com>
2022-12-30 15:41:44 +0300

    add changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <p>Shopping 2022 &copy;</p>
+    <div className='footer d-flex '
+    style={{
+      width:"100%",
+      fontSize:"19px",
+      justifyContent:"center",
+      alignItems:"center",
+      fontWeight:"bold",
+      height:"10vh",
+      position:"sticky",
+      top:"100%",
+
+    }}
+    >
+   
+    
+        <p>Store 2022 &copy;</p>
     </div>
   )
 }
